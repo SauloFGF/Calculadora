5174840c145cf4dc4be3127ffda8ca459ca7b63e
Saulo Figueiredo <saulo.fgf21@gmail.com>
2022-05-15 20:25:53 -0300

    Projeto Calculadora

---
+++ calculadora.js
@@ -0,0 +1,28 @@
+let elementoDosNumeros = document.querySelectorAll(".num")
+let display = document.querySelector(".viewer")
+let limpar = document.getElementById("clear")
+let operador = document.querySelectorAll(".ops")
+let resultado = document.querySelector(".equals")
+
+elementoDosNumeros.forEach((numElem) => {
+    numElem.onclick = () => {
+        display.textContent += numElem.textContent
+    }
+})
+
+limpar.onclick = () => {
+    display.textContent = ""
+}
+
+operador.forEach((opp) =>{
+    opp.onclick = () => {
+        display.textContent += opp.textContent
+        console.log(opp.textContent)
+    }
+} )
+
+resultado.onclick = () => {
+    if (display.textContent){
+        display.textContent = eval(display.textContent)
+    } 
+}
